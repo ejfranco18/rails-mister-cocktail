bf47260bf74dea4648c429f66efc9826060bec9a
Javier Franco <javier.franco18@gmail.com>
2019-11-11 17:57:57 +0100

    Update to the views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import 'bootstrap';
+import 'select2/dist/css/select2.css';
+
 import { showModal } from '../components/modal';
 
-import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/select'
+initSelect2();
 
 showModal();
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "cocktails#index"
   resources :cocktails do
+    post :index
     resources :doses
   end
 end
